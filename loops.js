798e14211f86a7b4baca3e759eda5e72475b775d
MarcelloVSilva <marcellovcs@gmail.com>
2018-10-05 16:51:53 -0300

    increase loops arr

--- loops.js
+++ loops.js
@@ -32,3 +32,31 @@ const withTax = orders.map(v => v * 1.1)
 
 // Filter
 const highValue = orders.filter(v => v > 100);
+
+/**
+ * Every
+ * @returns false
+ */
+const everyValueGreaterThan50 = orders.every(v => v > 50)
+
+/**
+ * Every
+ * @returns true
+ */
+const everyValueGreaterThan10 = orders.every(v => v > 10)
+
+
+/**
+ * Some
+ * @returns false
+ */
+const someValueGreaterThan500 = orders.some(v => v > 500)
+
+/**
+ * Some
+ * @returns true
+ */
+const someValueGreaterThan10 = orders.some(v => v > 10)
+
+
+
