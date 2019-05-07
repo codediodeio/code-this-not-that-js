3aef49d8e3178120837d475356eba05749466dd6
Clemens Peters <clemens.peters@honestfoodcompany.de>
2019-05-08 00:26:11 +0530

    CS fixes using   /Users/clemens/Downloads/code-this-not-that-js/async-await.js:33:7: Parsing error: Identifier 'sumRandomAsyncNums' has already been declared   /Users/clemens/Downloads/code-this-not-that-js/console-log.js:5:1: Expected an assignment or function call and instead saw an expression.   /Users/clemens/Downloads/code-this-not-that-js/console-log.js:11:1: Expected an assignment or function call and instead saw an expression.   /Users/clemens/Downloads/code-this-not-that-js/destructuring.js:19:10: Parsing error: Identifier 'feed' has already been declared   /Users/clemens/Downloads/code-this-not-that-js/loops.js:28:7: Parsing error: Identifier 'total' has already been declared   /Users/clemens/Downloads/code-this-not-that-js/rest-params.js:9:10: Parsing error: Identifier 'totalHitPoints' has already been declared   /Users/clemens/Downloads/code-this-not-that-js/spread-syntax.js:20:7: Parsing error: Identifier 'lvl0' has already been declared   /Users/clemens/Downloads/code-this-not-that-js/template-literals.js:8:1: Expected an assignment or function call and instead saw an expression.   /Users/clemens/Downloads/code-this-not-that-js/template-literals.js:12:1: Expected an assignment or function call and instead saw an expression. see: https://standardjs.com/.

--- console-log.js
+++ console-log.js
@@ -1,32 +1,28 @@
-const foo    = { name: 'tom',   age: 30, nervous: false };
-const bar    = { name: 'dick',  age: 40, nervous: false };
-const baz    = { name: 'harry', age: 50, nervous: true };
-
+const foo = { name: 'tom', age: 30, nervous: false }
+const bar = { name: 'dick', age: 40, nervous: false }
+const baz = { name: 'harry', age: 50, nervous: true }
 
 'Bad Code 💩'
 
-console.log(foo);
-console.log(bar);
-console.log(baz);
-
-
+console.log(foo)
+console.log(bar)
+console.log(baz)
 
 'Good Code ✅'
 
 // Computed Property Names
 
-console.log('%c My Friends', 'color: orange; font-weight: bold;' )
-console.log({ foo, bar, baz });
+console.log('%c My Friends', 'color: orange; font-weight: bold;')
+console.log({ foo, bar, baz })
 
 // Console.table(...)
 console.table([foo, bar, baz])
 
-
 // // Console.time
 console.time('looper')
 
-let i = 0;
-while (i < 1000000) { i ++ }
+let i = 0
+while (i < 1000000) { i++ }
 
 console.timeEnd('looper')
 
@@ -36,4 +32,3 @@ const deleteMe = () => console.trace('bye bye database')
 
 deleteMe()
 deleteMe()
-
